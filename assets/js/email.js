c2d9bce9cbf05c78c19f993ea2d43a8e1958d20a
Shamantha Gowda T S <89024736+shamanthagowda@users.noreply.github.com>
2023-12-09 16:17:00 +0530

    Add files via upload

--- assets/js/email.js
+++ assets/js/email.js
@@ -5,7 +5,7 @@ function Send(){
     var mess = document.getElementById("message").value;
 
 //     var body ='<table style="width: 100%;">'+ '<colgroup>'+
-//         '<col style="width: 33%; background: hsl(12, 90%, 49%); border-radius: 5px; border-color: white; padding: 0.25% 5px;">'+
+//         '<col style="width: 33%; background: hsl(12, 90%, 49%); test_align: center; border-radius: 5px; border-color: white; padding: 0.25% 5px;">'+
 //         '<col style="width: 66%; border-color: white; border-radius: 5px; padding: 0.25% 5px;">'+
 //     '</colgroup>'+
 //     '<tr><td> Name </td><td>'+name+"</td>'</tr>"+
@@ -13,12 +13,21 @@ function Send(){
 //     "<tr><td> Subject </td><td>"+sub +"</td></tr>"+
 //     "<tr><td> Message </td><td>"+mess+"</td></tr>"+
 // "</table>";
-
-    var body = "Name: " + name+ 
-    "<br/> Email :"+ email +
-    "<br/> Subject :" + sub+
-    "<br/> Message :"+ mess + 
-    "<br/>";
+       var body='<table style="width: 100%; background-color: aliceblue; border-collapse: collapse;">'+
+       '<colgroup>'+
+           '<col style="width: 33%;">'+
+           '<col style="width: 60%;">'+
+       '</colgroup>'+
+       '<tr><td style="color: blueviolet; padding: 10px 5px; border: solid black 2px;"> Name </td><td style="background-color: white; padding: 10px 5px; padding: 10px 5px; border: solid black 2px ;">'+ name +"</td></tr>"+
+       '<tr><td style="color: blueviolet; padding: 10px 5px; border: solid black 2px;"> Email </td><td style="background-color: white; padding: 10px 5px; padding: 10px 5px; border: solid black 2px ;">'+ email +"</td></tr>"+
+       '<tr><td style="color: blueviolet; padding: 10px 5px; border: solid black 2px;"> Subject </td><td style="background-color: white; padding: 10px 5px; padding: 10px 5px; border: solid black 2px ;">'+sub +"</td></tr>"+
+       '<tr><td style="color: blueviolet; padding: 10px 5px; border: solid black 2px;"> Message </td><td style="background-color: white; padding: 10px 5px;padding: 10px 5px; border: solid black 2px ; ">'+ messs +"</td></tr>"+
+   '</table>'
+    // var body = "Name: " + name+ 
+    // "<br/> Email :"+ email +
+    // "<br/> Subject :" + sub+
+    // "<br/> Message :"+ mess + 
+    // "<br/>";
 
     console.log(body);
   Email.send({
@@ -38,7 +47,7 @@ function Send(){
       //   button: "Ok"
       // });
     }
-    else{ alert("Failed to send. Please try again")
+    else{ alert("Failed to send. Please try again.")
       // swal({
       //   title: "Something went wrong",
       //   text: "Try again",
